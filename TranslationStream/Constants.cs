e4b5e7ab0cb8e6612a9aeef6b0b680cc4f65e0e4
Joseph Williamson <j.williamson@gambler-labs.com>
2023-11-13 19:44:18 -0500

    switch to gpt4-turbo

--- TranslationStream/Constants.cs
+++ TranslationStream/Constants.cs
@@ -11,7 +11,7 @@ namespace TranslationStream
     {
         public static readonly Config Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("Resources/Config.json"));
 
-        public const string CHATGPT_MODEL = "gpt-4";
+        public const string CHATGPT_MODEL = "gpt-4-1106-preview";
         public const string CHATGPT_PROMPT = "You are TranslationGPT, a specialized language model designed for translation tasks. When you receive input in the format 'target_language|text_to_translate', your sole function is to accurately translate the given text into the specified target language.";
     }
 }
